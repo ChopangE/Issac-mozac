0bf10a5ed40d1bc98bd5682eaacec758f5bfa94c
ChopangE <chosw05222@gmail.com>
2024-09-18 23:45:31 +0900

    Update Mage

---
+++ Project IM/Assets/Scripts/Player/Mage/MageWeapon.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageWeapon : PlayerWeapon
+{
+    // Start is called before the first frame update
+    public override void StartAttack()
+    {
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
