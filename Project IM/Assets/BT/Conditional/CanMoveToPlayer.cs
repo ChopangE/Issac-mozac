2ce7eb0c74edba7c264d9814e9611e74353003f0
ChopangE <chosw05222@gmail.com>
2024-09-24 19:22:45 +0900

    Update CanMoveToPlayer.cs

--- Project IM/Assets/BT/Conditional/CanMoveToPlayer.cs
+++ Project IM/Assets/BT/Conditional/CanMoveToPlayer.cs
@@ -19,8 +19,25 @@ public class CanMoveToPlayer : Conditional
     }
 
     public bool WithinSight(Transform targetTransform) {
-        Vector2 direction = targetTransform.position - transform.position;
-        float dist = direction.magnitude;
-        return dist <= distance;
+        Collider2D[] hit = Physics2D.OverlapCircleAll(transform.position, distance, LayerMask.GetMask("Player"));
+        if (hit.Length > 0)
+        {
+            target.Value = hit[0].transform;
+            float shortestDistance = (hit[0].transform.position - transform.position).magnitude;
+            for (int i = 0; i < hit.Length; i++)
+            {
+                if ((hit[i].transform.position - transform.position).magnitude < shortestDistance)
+                {
+                    target.Value = hit[i].transform;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+        // Vector2 direction = targetTransform.position - transform.position;
+        // float dist = direction.magnitude;
+        // return dist <= distance;
     }
 }
