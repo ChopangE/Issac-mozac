c62b3ec6551c44fff71cf0e10d6f71ca6a577675
ChopangE <chosw05222@gmail.com>
2024-09-26 18:25:00 +0900

    Update

--- Project IM/Assets/Scripts/UI/MainMenuViewModel.cs
+++ Project IM/Assets/Scripts/UI/MainMenuViewModel.cs
@@ -9,9 +9,22 @@ public class MainMenuViewModel : BaseViewModel
 {
     
     [Binding]
-    public void SelectClass()
+    public void SelectArcher()
     {
         Managers.StatManager.Classes = Define.Classes.Archer;
+        CallInGameScene();
+        
+    }
+    
+    [Binding]
+    public void SelectMage(){
+        Managers.StatManager.Classes = Define.Classes.Mage;
+        CallInGameScene();
+    }
+
+    public void CallInGameScene()
+    {
         Managers.SceneManager.LoadScene(Define.SceneType.InGame);
+
     }
 }
